eb067c82280a7981641c51110746ca259825aa93
PurpleBlueNL <50110305+PurpleBlueNL@users.noreply.github.com>
2023-06-01 12:21:33 +0200

    Create Generate_Empty_BinFile.py

---
+++ Generate_Empty_BinFile.py
@@ -0,0 +1,18 @@
+
+while True:
+  Size = input("Please enter size of bin file (MB):")
+  try:
+      num = int(Size)
+      print("Generating file of: " + str(num) + "MB")
+      break;
+  except ValueError:
+      try:
+        num = float(Size)
+        break;
+      except ValueError:
+        print("Input is not a number!")
+     
+FileName = str(num + ".bin")
+with open(FileName, "wb") as binfile:
+  binfile.write(b"x\00" * int(Size) * 1024 * 1024)
+  print("File generated!")
